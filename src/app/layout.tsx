2afa8fd8b7c17a5e71daa8419ba028685aba1cf2
Bimla-jain-555 <vj13798@gmail.com>
2025-02-25 10:03:00 +0530

    add nprogress bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import ClientLayout from "@/components/ClientLayout";
+
 
 export const metadata: Metadata = {
   title: "Arrow Trade",
@@ -19,15 +21,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body
-        className={`font-Inter bg-dark-black antialiased`}
-      >
-        {children}
+      <body className="font-Inter bg-dark-black antialiased">
+        <ClientLayout>  {children}</ClientLayout>
       </body>
     </html>
   );
